e167c0dcf748dd3254c4b7e4a9077c61e22f4e78
Evgenii Troinov <troinof@gmail.com>
2025-08-08 23:47:02 +0200

    master: Improve file_names_in

--- src/utils/file_names_in.rs
+++ src/utils/file_names_in.rs
@@ -5,5 +5,17 @@ use std::path::Path;
 pub fn file_names_in(dir: impl AsRef<Path>) -> io::Result<Vec<String>> {
     let mut names = Vec::new();
 
+    for entry in fs::read_dir(dir)? {
+        let entry = entry?;
+        let md = entry.metadata()?;
+
+        if md.is_file() {
+            match entry.file_name().to_str() {
+                Some(name) => names.push(name.to_string()),
+                None => println!("Error with file"),
+            }
+        }
+    }
+
     Ok(names)
 }
