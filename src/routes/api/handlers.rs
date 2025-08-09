7ad9a783e05572f7b15588ac9aed677aadd3f0e7
Evgenii Troinov <troinof@gmail.com>
2025-08-09 23:45:17 +0200

    master: Removed duplicates

--- src/routes/api/handlers.rs
+++ src/routes/api/handlers.rs
@@ -4,7 +4,15 @@ use crate::utils::file_names_in;
 
 pub async fn get_names() -> impl IntoResponse {
     match file_names_in("./assets/icons") {
-        Ok(files) => Json(files).into_response(),
+        Ok(files) => {
+            let unique_files: Vec<_> = files
+                .into_iter()
+                .collect::<std::collections::BTreeSet<_>>()
+                .into_iter()
+                .collect();
+
+            Json(unique_files).into_response()
+        }
         Err(_) => (StatusCode::NOT_FOUND, "Icon files not found").into_response(),
     }
 }
